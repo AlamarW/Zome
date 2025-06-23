8a02e868e08fdf16b5ab965396a9f882268c55fd
Austin <austin.wiggins@dualitysys.com>
2025-06-22 21:40:08 -0400

    finished refactoring NLP parts out of Notes

--- tests/test_nlpprocessor.py
+++ tests/test_nlpprocessor.py
@@ -7,3 +7,19 @@ def test_get_stop_words():
     processor = NLPProcessor()
 
     assert len(processor._stop_words) == 850
+
+
+def test_celan_text():
+    text = "Testing, test but this is fine. Fine."
+
+    clean_text = NLPProcessor.text_clean(text)
+
+    assert clean_text == "Testing test but this is fine Fine"
+
+
+def test_calculate_high_frequency_words():
+    text = "Testing test but this is fine Fine"
+    assert NLPProcessor.calculate_high_frequency_words(text) == {
+        "test": 2,
+        "fine": 2,
+    }  # returns most frequent used words, minus stop words

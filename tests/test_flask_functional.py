1408f4230960cc608db5a4b851264acc81e5c9ba
Austin <Austinlamarwiggins@gmail.com>
2025-06-27 21:47:48 -0400

    failing tests for GET messages for note endpoint

--- tests/test_flask_functional.py
+++ tests/test_flask_functional.py
@@ -40,4 +40,27 @@ def test_create_note_success(client):
     assert response_data["themes"] == ["sample", "note", "machine"]
 
 def test_get_note_by_name(client):
-    pass
+    """Test retrieving a note by name from db"""
+    payload = {
+            "name": "DB test Note",
+            "text": "This note will be stored in SQLite database for retriveal",
+            }
+
+    create_response = client.post(
+        "/note", data=json.dumps(payload), content_type="application/json"
+    )
+    assert create_response.status_code == 201
+
+    response = client.get("/note/DB test Note")
+    assert response.status_code == 200
+    response_data = json.loads(response.data)
+
+    assert response_data["name"] == "DB test Note"
+    assert response_data["text"] == payload["text"]
+    assert "id" in response_data
+    assert "created_at" in response_data
+    assert "updated_at" in response_data
+
+def test_get_nonexistent_note(client):
+    response = client.get("/note/NonExistent Note")
+    assert response.status_code == 404

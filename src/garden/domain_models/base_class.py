bed5e2cb8d4220655a0b1591fedbd5789c32cf8d
Austin <Austinlamarwiggins@gmail.com>
2025-07-05 22:42:24 -0400

    Some TDD cycles building out base class to ensure I need it, ending with a failing test case

--- src/garden/domain_models/base_class.py
+++ src/garden/domain_models/base_class.py
@@ -2,19 +2,13 @@ from typing import TYPE_CHECKING
 import uuid
 from datetime import datetime
 
-if TYPE_CHECKING:
-    from .tag import Tag
-
 
 class BaseClass:
     def __init__(self) -> None:
         self.uuid: uuid.UUID = uuid.uuid4()
         self.date_created: datetime = datetime.now()
         self.date_edited: datetime = datetime.now()
-        self.tags: list[str] = []
-
-    def set_uuid(self, uuid: uuid.UUID) -> None:
-        pass
+        self.tags: set[str] = []
 
     def get_uuid(self) -> uuid.UUID:
         return self.uuid
@@ -23,7 +17,7 @@ class BaseClass:
         pass
 
     def get_date_created(self) -> datetime:
-        pass
+        return self.date_created
 
     def set_date_edited(self, date: datetime) -> None:
         pass
@@ -31,16 +25,30 @@ class BaseClass:
     def get_date_edited(self) -> datetime:
         pass
 
-    def get_tags(self) -> list["Tag"]:
-        pass
+    def get_tags(self) -> list[str]:
+        return self.tags
+
+    @staticmethod
+    def _tag_formatter(tag: str) -> str:
+        if tag[0] != "#":
+            tag = f"#{tag}"
+
+        return tag.title()
 
     def set_tags(self, tags: str) -> None:
-        tags = tags.split()
-        tags = [f"#{tag.title()}" if tag[0] != "#" else f"{tag.title()}" for tag in tags]
-        self.tags = tags
+        if isinstance(tags, str):
+            tag_list = set(tags.split())
+            tag_list = {self._tag_formatter(tag) for tag in tag_list}
+            self.tags = tag_list
+        else:
+            raise TypeError
 
-    def add_tag(self, tag: "Tag") -> None:
-        pass
+    def add_tag(self, tag: str) -> None:
+        if isinstance(tag, str):
+            tag = self._tag_formatter(tag)
+            self.tags.add(tag)
+        else:
+            raise TypeError
 
     def remove_tag(self, tag: "Tag") -> None:
         pass
--- tests/test_base_class.py
+++ tests/test_base_class.py
@@ -1,16 +1,52 @@
 from garden.domain_models.base_class import BaseClass
-
+import uuid
+import pytest
 
 def test_set_tags():
     base = BaseClass()
 
     base.set_tags("#Test #Tags")
 
-    assert base.tags == ["#Test", "#Tags"]
+    assert base.tags == {"#Test", "#Tags"}
 
 def test_set_tags_w_formatting():
     base = BaseClass()
 
     base.set_tags("#Test Not formatted correctlY")
 
-    assert base.tags == ["#Test", "#Not", "#Formatted", "#Correctly"]
+    assert base.tags == {"#Test", "#Not", "#Formatted", "#Correctly"}
+
+def test_set_tags_w_snake_case():
+    base = BaseClass()
+
+    base.set_tags("#Snake_case_tag #Another_Snake_Case_Tag")
+
+    assert base.tags == {"#Snake_Case_Tag", "#Another_Snake_Case_Tag"}
+
+def test_add_tag():
+    base = BaseClass()
+    base.set_tags("#Existing #Tags")
+
+    base.add_tag("New_tag")
+
+    assert base.tags == {"#Existing", "#Tags", "#New_Tag"}
+
+def test_add_tag_not_correct_type():
+    base = BaseClass()
+
+    with pytest.raises(TypeError):
+        base.set_tags(1234)
+
+def test_remove_tag():
+    base = BaseClass()
+    base.set_tags("#Not_This_One #This_One")
+
+    base.remove_tag("#This_One")
+
+    assert base.tags == {"#Not_This_One"}
+
+
+def test_set_uuid():
+    base = BaseClass()
+
+    assert isinstance(base.uuid, uuid.UUID)
